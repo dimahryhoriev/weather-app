1d43d3adb12caac72517f30ffbda79eff1dd6a87
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-04-18 22:30:50 +0200

    fix: correct paths for fonts and images in CSS

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Mins (m * s * ms)
+    max: 25
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/api', require('./server/routes/weather'))
+
+// Enable cors
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
