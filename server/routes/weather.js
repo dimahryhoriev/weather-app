0957e64e1e3cc9a37a3031923e60384280e6ed1d
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-04-18 15:28:50 +0200

    feat: implement axios fetch with try/catch error handling

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-    res.json({ success: true })
+// Environment variables
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get('/', async (req, res) => {
+    try {
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE
+        })
+
+        const apiRes = await axios.get(`${API_BASE_URL}`);
+        const data = apiRes.data;
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 })
 
 module.exports = router;
