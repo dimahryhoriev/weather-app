6db66a320721b561139c46270e31601b585fe785
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-04-20 23:07:23 +0200

    fix: adjust text alignment for responsive design

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-let searchInput = document.querySelector('.dashboard__search-input');
-let searchBtn = document.querySelector('.dashboard__search-btn');
+let form = document.querySelector('.dashboard__search');
+let formInput = document.querySelector('.dashboard__search-input');
+let formBtn = document.querySelector('.dashboard__search-btn');
 let currentTemp = document.querySelector('[data-js="c-temp"]');
 let currentCity = document.querySelector('[data-js="c-city"]');
 let currentTime = document.querySelector('[data-js="c-time"]');
@@ -8,11 +9,15 @@ let currentDay = document.querySelector('[data-js="c-day"]');
 let currentMonth = document.querySelector('[data-js="c-month"]');
 let currentYear = document.querySelector('[data-js="c-year"]');
 
-function citySearch () {
-    searchInput.addEventListener ('enter', () => {
-        
-    })
+function citySearch(city) {
+    console.log(city);
 }
+
+form.addEventListener('submit', () => {
+    event.preventDefault();
+
+    console.log(formInput.value);
+})
 
 // Fetch weather data from API
 const fetchWeather = async (city) => {
